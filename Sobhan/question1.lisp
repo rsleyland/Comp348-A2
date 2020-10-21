dc78512953b0dd7ca5ad5aa6e7de4a59f50d6139
Sobhan-M <72103946+Sobhan-M@users.noreply.github.com>
2020-10-21 18:55:34 -0400

    Add files via upload

---
+++ Sobhan/question1.lisp
@@ -0,0 +1,16 @@
+(defun sublist (lst a b)
+    (cond
+        ((null lst) ())                                 ;condition 1: if the list is empty
+        ((> a b) ())                                    ;condition 2: start index is greater
+        ((or (minusp a) (minusp b)) ())                 ;condition 3: one of the indexes are negative
+        ((or (equal a 0) (> b (list-length lst))) ())   ;condition 4: a = 0 or b is out of bounds
+        ((and (equal b 0) (equal a 1)) ())              ;recursive end case: when b becomes 0
+        ((equal a 1) (cons (car lst) (sublist (cdr lst) a (- b 1))))    ;recursive: we reach start index
+        (t (sublist (cdr lst) (- a 1) (- b 1)))))       ;recursive: neither indexes are reached, so decrement
+
+;(print (sublist '(1 2 3 4 5 6) 2 3))
+;(print (sublist '(1 2 3 4 5 6) 1 6))
+;(print (sublist '(1 2 3 4 5 6) 0 3))
+;(print (sublist '(1 2 3 4 5 6) 3 2))
+;(print (sublist '(1 2 3 4 5 6) -1 2))
+;(print (sublist '(1 2 3 4 5 6) 2 10))
