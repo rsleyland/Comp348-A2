2d51d3557ed6efbffbb867776f9220cb35cdc384
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-22 16:47:56 -0400

    Create q4.lisp

---
+++ Ryan/q4.lisp
@@ -0,0 +1,7 @@
+(defun is-Bin-Tree(lst)
+    (if (null lst) t
+    (cond ((or (null (car (cdr lst))) (null (car (cdr (cdr lst)))))  )
+          ((or (< (car lst) (car (car (cdr lst)))) (> (car lst) (car (car (cdr (cdr lst)))))) nil)
+          (t (progn (is-Bin-Tree (car (cdr lst))) (is-Bin-Tree (car (cdr (cdr lst)))))))))
+
+;(write (is-Bin-Tree '(8 (5 (4) (7)) (11 (6) (12)))))
