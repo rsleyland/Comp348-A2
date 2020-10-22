7bb2bbe5e13a71bfbe4ef5e252d771311b6ea623
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-22 17:10:27 -0400

    Update q4.lisp

--- Ryan/q4.lisp
+++ Ryan/q4.lisp
@@ -1,7 +1,8 @@
 (defun is-bst(lst)
     (if (null lst) t
-    (cond ((or (null (car (cdr lst))) (null (car (cdr (cdr lst)))))  )
-          ((or (< (car lst) (car (car (cdr lst)))) (> (car lst) (car (car (cdr (cdr lst)))))) nil)
-          (t (progn (is-bst (car (cdr lst))) (is-bst (car (cdr (cdr lst)))))))))
+    (cond ((and (null (car (cdr lst))) (null (car (cdr (cdr lst))))) )
+          ((< (car lst) (car (car (cdr lst)))) nil)
+          ((> (car lst)  (car (car (cdr (cdr lst))))) nil)
+          (t (progn (and (is-bst (car (cdr lst))) (is-bst (car (cdr (cdr lst))))))))))
 
-;(write (is-bst '(8 (5 (4) (7)) (11 (9) (12)))))
+(write (is-bst '(8 (4 (3) (8)) (11 (7) (12)))))
