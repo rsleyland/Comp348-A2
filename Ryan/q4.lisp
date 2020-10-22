194cb6cf8f850895f74c1b41a7d950924751d656
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-22 16:49:39 -0400

    Update q4.lisp

--- Ryan/q4.lisp
+++ Ryan/q4.lisp
@@ -4,4 +4,4 @@
           ((or (< (car lst) (car (car (cdr lst)))) (> (car lst) (car (car (cdr (cdr lst)))))) nil)
           (t (progn (is-bst (car (cdr lst))) (is-bst (car (cdr (cdr lst)))))))))
 
-;(write (is-Bin-Tree '(8 (5 (4) (7)) (11 (9) (12)))))
+;(write (is-bst '(8 (5 (4) (7)) (11 (9) (12)))))
