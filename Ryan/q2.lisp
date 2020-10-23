3f54f7edbe0ce73fffa54a180bd2a21a13754f92
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-22 22:28:01 -0400

    Update q2.lisp

--- Ryan/q2.lisp
+++ Ryan/q2.lisp
@@ -3,7 +3,9 @@
         (if (eql b nil) (setf b a))
         (if (or (< b 1) (< a 1) (> b (list-length lst))) nil
     (cond 
-        ((> a b) (progn (setq temp a) (setf a (+ b (- (length lst) temp))) (setf b (+ temp (- (length lst) temp) )) (setf lst (reverse lst)) (sub-list2 lst a b)))
+        ((> a b) (progn 
+                     (setq temp a) (setf a (+ b (- (length lst) temp))) (setf b (+ temp (- (length lst) temp) )) 
+                     (setf lst (reverse lst)) (sub-list2 lst a b)))
         ((> a 1) (sub-list2 (cdr lst) (- a 1) (- b 1)))
         ((<= a b) (cons (car lst) (sub-list2 (cdr lst) a (- b 1)))))))
 
