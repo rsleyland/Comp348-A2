389b4e9ea17fec6f9bd816f1ba0101001989f0e6
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-22 21:56:46 -0400

    Update q2.lisp

--- Ryan/q2.lisp
+++ Ryan/q2.lisp
@@ -1,11 +1,11 @@
 (defun sub-list2(lst &optional (a 1) (b (list-length lst)))
         (if (or (< b 1) (< a 1) (> b (list-length lst))) nil
     (cond 
-        ((> a b) (progn (setq temp a) (setq a b) (setq b temp) (sub-list2 lst a b)))
+        ((> a b) (progn (setf temp a) (setf a b) (setf b temp) (setf lst (reverse lst)) (sub-list2 lst a b)))
         ((> a 1) (sub-list2 (cdr lst) (- a 1) (- b 1)))
         ((<= a b) (cons (car lst) (sub-list2 (cdr lst) a (- b 1)))))))
 
 ;(write (sub-list2 '(a b c d) 1 2))
 
-;need to add 
+;need to fix 
 ;(sub-list '(1 6 12) nil 1)
