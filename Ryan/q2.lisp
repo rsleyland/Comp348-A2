99b670885b17414611d3f2fc017c2fca3487690e
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-22 23:11:35 -0400

    Update q2.lisp

--- Ryan/q2.lisp
+++ Ryan/q2.lisp
@@ -1,13 +1,14 @@
-(defun sub-list2(lst &optional (a 1) (b (list-length lst)))
+(defun sub-list(lst &optional (a 1) (b (list-length lst)))
         (if (and (eql a nil) (eql b nil)) (progn (setf a 0) (setf b 0)))
         (if (eql a nil) (setf a b))
         (if (eql b nil) (setf b a))
         (if (or (< b 1) (< a 1) (> b (list-length lst)) ) nil
     (cond 
         ((> a b) (progn 
-                     (setq temp a) (setf a (+ b (- (length lst) temp))) (setf b (+ temp (- (length lst) temp) )) 
-                     (setf lst (reverse lst)) (sub-list2 lst a b)))
-        ((> a 1) (sub-list2 (cdr lst) (- a 1) (- b 1)))
-        ((<= a b) (cons (car lst) (sub-list2 (cdr lst) a (- b 1)))))))
+                     (setq tempa a) (setq tempb b) (setf b (+ tempa 1 (- (length lst) tempa tempb))) (setf a (+ tempb 1 (- (length lst) tempa tempb)))
+                     (setf lst (reverse lst)) (sub-list lst a b)))
+        ((> a 1) (sub-list (cdr lst) (- a 1) (- b 1)))
+        ((<= a b) (cons (car lst) (sub-list (cdr lst) a (- b 1)))))))
 
-;(write (sub-list2 '(a b c d) 1 2))
+
+;(write (sub-list '(1 6 12 15 16) 6 1))
