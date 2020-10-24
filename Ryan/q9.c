904121cedc877da91e7b47af502eee0795d5133b
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-24 12:21:38 -0400

    Update q9.c

--- Ryan/q9.c
+++ Ryan/q9.c
@@ -1,23 +1,22 @@
 #include  <stdio.h>
 
-int* findMin(int* arr, int size)
-{
-    int min = arr[0];
-    int index = 0;
-    for (size; size>1; size--){
-        if (arr[size-1]<min){
-            min = arr[size-1];
-            index = size-1;
-        }
-    }
-     return arr[index];
-}
-
+int* findMin(int* arr, int size);
 
 int main(void)
 {
-    int arr[] = {-23, 4, -12, 5, 6, -1, -4};
+    int arr[] = {2, 4, -12, 5, 6, -1, -4};
     int * m =findMin(arr, 7);
-    printf("%d\n", m);
+    printf("%d\n", *m);
     return 0;
+}
+
+int* findMin(int* arr, int size)
+{
+    int min = 0;
+    for (size; size>1; size--){
+        if (arr[size-1]<arr[min]){
+            min = size-1;
+        }
+    }
+     return &arr[min];
 }
