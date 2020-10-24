78b89a8fa91e6140db324e8d4df039905036ced8
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-24 11:10:41 -0400

    Create q8.lisp

---
+++ Ryan/q8.lisp
@@ -0,0 +1,9 @@
+(defun lucas_ (n)
+    (cond ((= n 0) ())
+          ((= n 1) '(2))
+          ((= n 2) '(1 2))
+          (t (cons (+ (car(cdr(lucas_ (- n 1)))) (car(lucas_ (- n 1)))) (lucas_ (- n 1))))))
+(defun lucas (n)
+    (reverse (lucas_ n)))
+     
+;(write (lucas 5))
