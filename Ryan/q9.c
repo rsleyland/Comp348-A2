15d50a58f40960d95f4fc1c87b1a4a2e9b27cbaf
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-24 21:31:02 -0400

    Update q9.c

--- Ryan/q9.c
+++ Ryan/q9.c
@@ -1,22 +1,27 @@
 #include  <stdio.h>
 
-int* findMin(int* arr, int size);
+int* findmin(int* arr, int size){
+    int *min = arr; //set min pointer to first element of array
+    int *iter =arr+1; //set iterator to second element of array
+    for(;iter<&arr[size]; iter++){  //address of arr[size] will be out of bounds so will iterator until one before this
+        if(*iter<*min) min=iter; //compares if value iterator is pointing at is less than value min is pointing at, ->sets min pointer to new value
+    }
+    return min; //returns pointer to min value after iterating through full array
+}
+
+int* findmin2(int arr[], int size){ //works same as above but use int[] as param rather than auto casting int[] to int*
+    int *min = &arr;
+    int *iter =&arr[1];
+    for(;iter<&arr[size]; iter++){
+        if(*iter<*min) min=iter;
+    }
+    return min;
+}
 
 int main(void)
 {
-    int arr[] = {2, 4, -12, 5, 6, -1, -4};
-    int * m =findMin(arr, 7);
-    printf("%d\n", *m);
+    int arr[] = {1, 4, 5, 6, -1, -12};
+    int *min =findmin(arr, 6);
+    printf("%d", *min);
     return 0;
-}
-
-int* findMin(int* arr, int size)
-{
-    int min = 0;
-    for (size; size>1; size--){
-        if (arr[size-1]<arr[min]){
-            min = size-1;
-        }
-    }
-     return &arr[min];
 }
