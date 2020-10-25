aacdaffebdce182c587f6098a6eefd1d11c75c3e
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-24 21:31:46 -0400

    Update q9.c

--- Ryan/q9.c
+++ Ryan/q9.c
@@ -3,8 +3,8 @@
 int* findmin(int* arr, int size){
     int *min = arr; //set min pointer to first element of array
     int *iter =arr+1; //set iterator to second element of array
-    for(;iter<&arr[size]; iter++){  //address of arr[size] will be out of bounds so will iterator until one before this
-        if(*iter<*min) min=iter; //compares if value iterator is pointing at is less than value min is pointing at, ->sets min pointer to new value
+    for(;iter<&arr[size]; iter++){  //address of arr[size] will be out of bounds so will iterate until element with address one before this
+        if(*iter<*min) min=iter; //compare if value iterator is pointing < value min is pointing at, ->sets min pointer to new value
     }
     return min; //returns pointer to min value after iterating through full array
 }
