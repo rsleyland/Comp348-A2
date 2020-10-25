fb7882f764c6fb3607ea0419ceb2ce90c441e9be
Sobhan-M <72103946+Sobhan-M@users.noreply.github.com>
2020-10-25 19:51:13 -0400

    changed to remove duplicates

--- Sobhan/question3.lisp
+++ Sobhan/question3.lisp
@@ -5,13 +5,20 @@
 ;                   2) otherwise attach the head to the recursive operation on the tail
 
 (defun straight (lst)
-
     (cond
-
         ((null lst) ())
-
         ((listp (car lst)) (append (straight (car lst)) (straight (cdr lst))))
+        (t 
+            (setq duplicates nil)
+            (remove_dup (cons (car lst) (straight (cdr lst)))))))
 
-        (t (cons (car lst) (straight (cdr lst))))))
+(defun remove_dup (lst)
+    (cond
+        ((null lst) nil)
+        ((member (car lst) duplicates) (remove_dup (cdr lst)))
+        ((member (car lst) (cdr lst)) 
+            (setq duplicates (cons (car lst) duplicates))
+            (cons (car lst) (remove_dup (cdr lst))))
+        (t (cons (car lst) (remove_dup (cdr lst))))))
 
-; (print (straight '(1 (1 2 3) b (4 (3 2)))))
+;(print (straight '(1 (1 2 3) b (4 (3 2)))))
