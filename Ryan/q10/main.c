a744b5d7ee4a836c1da83acc38ac44b216c1ba48
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-24 23:24:03 -0400

    Update main.c

--- Ryan/q10/main.c
+++ Ryan/q10/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "selectionsort.h"
-#include "selectionsort.h"
+#include "selectionsort.h" //included twice 
 int main(void)
 {
     int arr[] = {1, 4, 0, -45, 6, -1, -12};
@@ -9,3 +9,7 @@ int main(void)
     for (a=0; a<7; a++) printf("%d ", arr[a]);
     return 0;
 }
+
+// TO COMPILE 
+//-> gcc selectionsort.c main.c 
+//-> ./a.out
