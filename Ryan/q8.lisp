c7edabb249e7d3a5309e57ee4a09f99297f6f1c0
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-29 12:13:35 -0400

    Update q8.lisp

--- Ryan/q8.lisp
+++ Ryan/q8.lisp
@@ -2,7 +2,7 @@
     (cond ((< n 1) '())
           ((= n 1) '(2))
           ((= n 2) '(1 2))
-          (t (cons (+ (cadr(lucas_ (- n 1))) (car(lucas_ (- n 1)))) (lucas_ (- n 1))))))
+          (t (cons (+ (car(lucas_ (- n 1))) (cadr(lucas_ (- n 1)))) (lucas_ (- n 1))))))
 (defun lucas (n)
     (reverse (lucas_ n)))
      
