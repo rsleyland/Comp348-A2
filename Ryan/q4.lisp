7ae12d8f423ce727a1ba1a15603658716f343e7b
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-29 11:40:31 -0400

    Update q4.lisp

--- Ryan/q4.lisp
+++ Ryan/q4.lisp
@@ -5,4 +5,4 @@
         ((and (numberp (car(car (cdr (cdr lst))))) (>= (car lst) (car(car (cdr (cdr lst)))))) nil)
         (t (and (is-bst (car (cdr lst))) (is-bst (cdr (cdr lst)))))))
 
-;write (is-bst '(8 (3 (1 () ()) (6 (4 () ())(7 () ()))) (10 () (14 (13 () ()) ())))))
+;(write (is-bst '(8 (3 (1 () ()) (6 (4 () ())(7 () ()))) (10 () (14 (13 () ()) ())))))
