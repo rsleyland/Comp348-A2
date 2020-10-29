787f456cf281d38dbcfe3a1ab4e7297346f43707
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-29 11:40:03 -0400

    Update q4.lisp

--- Ryan/q4.lisp
+++ Ryan/q4.lisp
@@ -1,14 +1,8 @@
-(defun is-bst(lst root)
-    (cond ((null (car lst)) t)
-          ((and (null (second lst)) (null (third lst))) )
-          ((and (null (second lst)) (< (car(third lst)) (car lst))) nil)
-          ((and (null (third lst)) (> (car (second lst)) (car lst))) nil)
-          ((and (not(null (second lst))) (not(null (third lst))) (< (car lst) root) (or (> (car(third lst)) root) (> (car(second lst)) (car lst)) (< (car (third lst)) (car lst)))) nil)
-          ((and (not(null (car(third lst)))) (not(null (car(second lst)))) (> (car lst) root) (or (< (car (second lst)) root) (< (car (third lst)) (car lst)) (> (car (second lst)) (car lst)))) nil)
-          (t (and (is-bst (second lst) root) (is-bst (third lst) root)))))
+(defun is-bst(lst)
+    (cond
+        ((null lst) t)
+        ((and (numberp (car (car (cdr lst)))) (< (car lst) (car (car (cdr lst))))) nil)
+        ((and (numberp (car(car (cdr (cdr lst))))) (>= (car lst) (car(car (cdr (cdr lst)))))) nil)
+        (t (and (is-bst (car (cdr lst))) (is-bst (cdr (cdr lst)))))))
 
-(defun is-bst_(lst)
-    (let ((root (car lst)))
-     (is-bst lst root)))
-    
-(write (is-bst_ '(8 (3 (1 (1) ()) (6 (4 () ())(7 () ()))) (10 () (14 (13 () ()) ())))))
+;write (is-bst '(8 (3 (1 () ()) (6 (4 () ())(7 () ()))) (10 () (14 (13 () ()) ())))))
