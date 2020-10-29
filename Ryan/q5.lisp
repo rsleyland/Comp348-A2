96bef1b3c7c9c4ac6f638b50a061a40eafad550a
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-29 12:09:59 -0400

    Update q5.lisp

--- Ryan/q5.lisp
+++ Ryan/q5.lisp
@@ -1,9 +1,9 @@
 (defun split (lst)
     (if (not (listp lst)) nil
-    (let ((lst1 ()))
-         (loop while (< (length lst1) (length lst)) do
-               (setf lst1 (append lst1 (list (car lst))))
-               (setf lst (cdr lst)))
-         (list lst1 lst))))
+    (let ((lst1 ()) (lst2 lst))
+         (loop while (< (length lst1) (length lst2)) do
+               (setf lst1 (append lst1 (list (car lst2))))
+               (setf lst2 (cdr lst2)))
+         (list lst1 lst2))))
 
 ;(write (split '(1 (2 3) 4 5 6)))
