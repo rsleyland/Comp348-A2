dda567730ee0d26167118b8981487022900687fc
p-carrier <p-carrier@users.noreply.github.com>
2020-11-02 23:11:40 -0500

    Create Q8.lisp

---
+++ z_Final/Q8.lisp
@@ -0,0 +1,9 @@
+(defun lucas_(n) 
+    (cond
+        ((or (not (integerp n)) (<= n 0)) nil) ;lucas[0] = 2 & lucas[1] = 1
+        ((= n 1) (list 2))
+        ((= n 2) (list 1 2))
+        (t (cons (+ (car (lucas_ (1- n))) (cadr (lucas_ (1- n)))) (lucas_ (1- n))))))
+
+(defun lucas(n)
+    (reverse (lucas_ n)))
