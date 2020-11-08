de3e591d018b2945df96064f3ab9e7d6d6353c69
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-11-08 13:53:41 -0500

    Update q9.c

--- Ryan/q9.c
+++ Ryan/q9.c
@@ -1,19 +1,10 @@
 #include  <stdio.h>
 
-int* findmin(int* arr, int size){
-    int *min = arr; //set min pointer to first element of array
-    int *iter =arr+1; //set iterator to second element of array
-    for(;iter<&arr[size]; iter++){  //address of arr[size] will be out of bounds so will iterate until element with address one before this
-        if(*iter<*min) min=iter; //compare if value iterator is pointing < value min is pointing at, ->sets min pointer to new value
-    }
-    return min; //returns pointer to min value after iterating through full array
-}
-
-int* findmin2(int arr[], int size){ //works same as above but use int[] as param rather than auto casting int[] to int*
-    int *min = &arr;
-    int *iter =&arr[1];
-    for(;iter<&arr[size]; iter++){
-        if(*iter<*min) min=iter;
+int * findmin(int * arr, int size){
+    int * min = arr;
+    int * iter = arr+1;
+    for(; iter<&arr[size]; iter++){
+        if(*iter<*min) min = iter;
     }
     return min;
 }
@@ -22,6 +13,6 @@ int main(void)
 {
     int arr[] = {1, 4, 5, 6, -1, -12};
     int *min =findmin(arr, 6);
-    printf("%d", *min);
+    printf("%d\n", *min);
     return 0;
 }
