c6f072459cf79717fa893a453664ddb740a7bf96
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-11-08 13:58:17 -0500

    Update selectionsort.h

--- Ryan/q11/selectionsort.h
+++ Ryan/q11/selectionsort.h
@@ -1,3 +1,8 @@
-void selectionsort(int*, int, int* (int*, int));
-int* findmin(int*, int);
-int* findmax(int*, int);
+#ifndef A2_SELECTIONSORT_V2_H
+#define A2_SELECTIONSORT_V2_H
+
+void selectionsort(int* arr, int size, int* (*func1)(int*, int));
+int* findmin(int* arr, int size);
+
+#endif //A2_SELECTIONSORT_V2_H
+
