1367378f96c19921f7c0a0537ff488f2b759db7c
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-11-11 17:54:59 -0500

    Update Q3.lisp

--- z_Final/Q3.lisp
+++ z_Final/Q3.lisp
@@ -1,23 +1,14 @@
-(defun flat (lst)
-    "flatten a list of many type"
-    (setf x nil)
-    (loop for y in lst
-            if (listp y)
-            do (setf x (append x (flatten-numbers y)))
-            if (numberp y)
-            do (setf x (cond ((not x) (list y))
-                                (t (append x (list y))))
-            )
-            finally (return x)
-          )
-)
+(defun flatten(lst)
+    (cond
+        ((or (not (listp lst)) (null lst)) nil)
+        ((listp (car lst)) (append (flatten (car lst)) (flatten (cdr lst))))
+        ((numberp (car lst)) (remove-dupes (reverse (cons (car lst) (flatten (cdr lst))))))
+        (t (flatten (cdr lst)))))
 
-(defun flatten-numbers (lst)
-    (setf flst (flat lst))
-    (setf nlst (list (first flst)))
-    (loop for i in flst
-            when (not (find i nlst))
-            do (setf nlst (append nlst (list i)))
-            finally (return nlst)
-            )
-)
+(defun remove-dupes(lst)
+    (cond
+        ((null lst) nil)
+        ((member (car lst) (cdr lst)) (remove-dupes(cdr lst)))
+        (t (append (remove-dupes (cdr lst)) (list(car lst))))))
+
+;(write (flatten '(1 2 (3 1) (a 2.5) (2 4.5) ((1 2)))))
