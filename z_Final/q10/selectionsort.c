b8dd4db9012d2a4419ed45b984f75e363660340c
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-11-11 18:15:13 -0500

    Update selectionsort.c

--- z_Final/q10/selectionsort.c
+++ z_Final/q10/selectionsort.c
@@ -1,16 +1,15 @@
-static int* findmin(int* arr, int size){
-    int *min = arr; //set min pointer to first element of array
-    int *iter =arr+1; //set iterator to second element of array
-    for(;iter<&arr[size]; iter++){  //address of arr[size] will be out of bounds so will iterate until element with address one before this
-        if(*iter<*min) min=iter; //compare if value iterator is pointing < value min is pointing at, ->sets min pointer to new value
+static int * findmin(int * arr, int size){
+    int * min = arr;
+    int * iter = arr+1;
+    for(; iter<&arr[size]; iter++){
+        if(*iter<*min) min = iter;
     }
-    return min; //returns pointer to min value after iterating through full array
+    return min;
 }
 
-void selectionsort(int* arr, int size){
+void selectionsort(int* arr, int size) {
     int count_unsorted = size;
-    int a; //increment counter - will not have to loop to the last value as will already be sorted at this point if last to be picked
-    for (a=0; a<size; a++){
+    for (int a = 1; a < size; a++) {
         int *min = findmin(arr, count_unsorted); //get min
         int temp = *arr; //store contents of pointer which will always be pointing at head of unsorted sublist
         *arr = *min;     //move min at start of unsorted sublist AKA end of sorted sublist
@@ -18,18 +17,4 @@ void selectionsort(int* arr, int size){
         arr++;           //increment arr pointer to head of unsorted sublist
         count_unsorted--;//keep track of how many elements in unsorted sublist
     }
-}
-
-
-// They are similar but choose which one you like best
-void selectionsort(int *arr, int size)
-{
-  for (int i = 0; i < size; i++)
-  {
-    int *el = arr + i;
-    int *min = findmin(el, size - i);
-    int temp = *min;
-    *min = *el;
-    *(arr + i) = temp;
-  }
 }
