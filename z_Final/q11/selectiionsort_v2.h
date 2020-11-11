38c809768b476562747f1cb680d28fa54fbf3c28
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-11-11 18:16:56 -0500

    Create selectiionsort_v2.h

---
+++ z_Final/q11/selectiionsort_v2.h
@@ -0,0 +1,7 @@
+#ifndef A2_SELECTIONSORT_V2_H
+#define A2_SELECTIONSORT_V2_H
+
+void selectionsort(int* arr, int size, int* (*func1)(int*, int));
+int* findmin(int* arr, int size);
+
+#endif //A2_SELECTIONSORT_V2_H
